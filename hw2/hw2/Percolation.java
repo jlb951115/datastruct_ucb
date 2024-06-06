1df72c072cf99c2d35fd2202684dcdf3768d48a6
jlb951115 <3065649450@qq.com>
2024-06-06 10:46:06 +0800

    completed the hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,75 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int Limation;
+    private int[][] Array;
+    private WeightedQuickUnionUF Set;
+    private int count;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+        Array = new int[N][N];
+        Limation = N;
+        Set = new WeightedQuickUnionUF(N * N + 2);
+        for (int i = 0; i < N; i++) {
+            Set.union(i, N * N);
+        }
+        for (int i = 0; i < N; i++) {
+            Set.union(N * (N - 1) + i, N * N + 1);
+        }
+        count = 0;
+    }
 
+    private int getIndex(int row, int col) {
+        return row * Limation + col;
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row >= Limation || col >= Limation) {
+            throw new IndexOutOfBoundsException("Out of Bounds");
+        }
+        return Array[row][col] == 1;
+    }
+
+    public int numberOfOpenSites() {
+        return count;
+    }
+
+    public boolean percolates() {
+        int x = Limation * Limation;
+        return Set.connected(x, x + 1);
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row >= Limation || col >= Limation) {
+            throw new IndexOutOfBoundsException("Out of Bounds");
+        }
+        return Set.connected(getIndex(row, col), Limation * Limation) && isOpen(row, col);
+    }
+
+    public void open(int row, int col) {
+        if (row >= Limation || col >= Limation) {
+            throw new IndexOutOfBoundsException("Out of Bounds");
+        }
+        Array[row][col] = 1;
+        count += 1;
+        int index = getIndex(row, col);
+        for (int i = row - 1; i <= row + 1; i += 2) {
+            if (i < 0 || i >= Limation) {
+                continue;
+            }
+            if (isOpen(i, col)) {
+                Set.union(getIndex(i, col), index);
+            }
+        }
+        for (int i = col - 1; i <= col + 1; i += 2) {
+            if (i < 0 || i >= Limation) {
+                continue;
+            }
+            if (isOpen(row, i)) {
+                Set.union(getIndex(row, i), index);
+            }
+        }
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,55 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
 
+public class PercolationStats {
+    private Percolation[] Experiments;
+    private int T;
+    private int N;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+        this.N = N;
+        this.T = T;
+        Experiments = new Percolation[T];
+        for (int i = 0; i < T; i++) {
+            Experiments[i] = pf.make(N);
+        }
+        for (int i = 0; i < T; i++) {
+            while (!Experiments[i].percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                if (!Experiments[i].isOpen(row, col)) {
+                    Experiments[i].open(row, col);
+                }
+            }
+        }
+    }
+
+    public double mean() {
+        double mean = 0.0;
+        for (int i = 0; i < T; i++) {
+            mean += (double)(Experiments[i].numberOfOpenSites() / N / N);
+        }
+        return mean / T;
+    }
+
+    public double stddev() {
+        double m = this.mean();
+        double std = 0.0;
+        for (int i = 0; i < T; i++) {
+            double temp = (double)(Experiments[i].numberOfOpenSites() / N / N)- m;
+            std = std + temp * temp;
+        }
+        return std / (T - 1);
+    }
+
+    public double confidenceLow() {
+        return this.mean() - (1.96 * Math.sqrt(this.stddev()) / Math.sqrt(T));
+    }
+
+    public double confidenceHigh() {
+        return this.mean() + (1.96 * Math.sqrt(this.stddev()) / Math.sqrt(T));
+    }
 }
